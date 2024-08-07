76dee4dc101020e2ce88f8802addc49d546d4f51
swalletsugarteam <sugargoose20@gmail.com>
2024-08-07 23:14:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -508,6 +508,7 @@ if (startPage) {
                     },
                     body: JSON.stringify({ user_id, username })
                 })
+                console.log('fetched ?')
                 window.location.href = 'https://swalletsugarteam.github.io/swallet_sugarteam/create_wallet/';
             });
         }
